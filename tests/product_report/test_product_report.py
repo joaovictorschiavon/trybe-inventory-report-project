8d52dd8370a6f03e543b671643efb692ba9f3438
João Victor Schiavon <schiavonj@yahoo.com.br>
2023-05-09 20:29:33 -0300

    feat: requisite 08

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,24 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+product_mock = {
+    "id": 10,
+    "nome_do_produto": "anel de poder",
+    "nome_da_empresa": "Sauron ltda",
+    "data_de_fabricacao": "2019-04-15",
+    "data_de_validade": "2022-06-22",
+    "numero_de_serie": "012345678",
+    "instrucoes_de_armazenamento": "no fundo de lagos",
+}
+
+report = (
+    f"O produto {product_mock['nome_do_produto']}"
+    f" fabricado em {product_mock['data_de_fabricacao']}"
+    f" por {product_mock['nome_da_empresa']}"
+    f" com validade até {product_mock['data_de_validade']}"
+    f" precisa ser armazenado {product_mock['instrucoes_de_armazenamento']}."
+)
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    prod = Product(**product_mock)
+    assert repr(prod) == report
