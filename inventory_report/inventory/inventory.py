d61c1b9ccf997e0815f82d5c328919a9520e043a
João Victor Schiavon <schiavonj@yahoo.com.br>
2023-05-09 18:12:05 -0300

    feat: requisite 05 and 6

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,16 +1,39 @@
 from ..reports.simple_report import SimpleReport
 from ..reports.complete_report import CompleteReport
 import csv
+import json
+import xmltodict
+import os
 
 
 class Inventory:
     @staticmethod
-    def import_data(path, type):
+    def json_data(file):
+        list = json.load(file)
+        return list
+
+    @staticmethod
+    def data_type(path, file_extension):
         data_list = []
         with open(path, "r") as file:
-            data_reader = csv.DictReader(file, delimiter=",")
-            for row in data_reader:
-                data_list.append(row)
+            if file_extension == ".csv":
+                data_reader = csv.DictReader(file, delimiter=",")
+                for row in data_reader:
+                    data_list.append(row)
+
+            if file_extension == ".json":
+                data_list = Inventory.json_data(file)
+
+            if file_extension == ".xml":
+                pre_list = xmltodict.parse(file.read())
+                data_list = pre_list["dataset"]["record"]
+        return data_list
+
+    @staticmethod
+    def import_data(path, type):
+        filename, file_extension = os.path.splitext(path)
+
+        data_list = Inventory.data_type(path, file_extension)
 
         if type == "simples":
             return SimpleReport.generate(data_list)
